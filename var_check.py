804e7b35df1c949b16272d27944b202e5197978c
vrush119 <vrushali.kudanekar@gmail.com>
2025-05-05 23:22:00 +0530

    Add files via upload

---
+++ var_check.py
@@ -0,0 +1,20 @@
+print("python has 3 var type:- integer,float,complex")
+myval=1
+print(type(myval))
+print(str(myval) + " is of the data type " + str(type(myval)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
